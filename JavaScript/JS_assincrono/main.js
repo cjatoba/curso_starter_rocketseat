f2d65686f7fe20d882713d3c8d367fc049089eef
clayton jatoba dos santos <cjsantos@saovicente.intra>
2020-08-23 16:47:53 -0300

    Utilizando Axios - Simplificando a forma de requisição

--- JavaScript/JS_assincrono/main.js
+++ JavaScript/JS_assincrono/main.js
@@ -1,3 +1,5 @@
+
+//-------------------------Utilizando XMLHttpRequest
 // var xhr = new XMLHttpRequest();
 
 // xhr.open('GET', 'https://api.github.com/users/cjatoba');
@@ -8,37 +10,55 @@
 //         console.log(JSON.parse(xhr.responseText))
 //     }
 // }
+//-------------------------Utilizando XMLHttpRequest
 
+//-------------------------Utilizando Promises
 //Promises
-function myPromise(){
-    //o parâmetro resolve é um função e recebe o retorno da requisição caso tenha sucesso
-    //o parâmetro reject é uma função e recebe o retorno da requisição caso tenha falha
-    return new Promise(function(resolve, reject){
-        var xhr = new XMLHttpRequest();
+// function myPromise(){
+//     //o parâmetro resolve é um função e recebe o retorno da requisição caso tenha sucesso
+//     //o parâmetro reject é uma função e recebe o retorno da requisição caso tenha falha
+//     return new Promise(function(resolve, reject){
+//         var xhr = new XMLHttpRequest();
 
-        xhr.open('GET', 'https://api.github.com/users/cjatoba');
-        xhr.send(null);
+//         xhr.open('GET', 'https://api.github.com/users/cjatoba');
+//         xhr.send(null);
 
-        xhr.onreadystatechange = function(){
-            //Verifica o retorno do Ajax
-            if(xhr.readyState === 4){
-                //Verifica se o retorno do Ajax foi ok (status = 200)
-                if(xhr.status === 200){
-                    resolve(JSON.parse(xhr.responseText))
-                }else {
-                    reject('Erro na requisição');
-                }
-            }
-        }
-    });
-}
+//         xhr.onreadystatechange = function(){
+//             //Verifica o retorno do Ajax
+//             if(xhr.readyState === 4){
+//                 //Verifica se o retorno do Ajax foi ok (status = 200)
+//                 if(xhr.status === 200){
+//                    //Converte JSON em um objeto
+//                     resolve(JSON.parse(xhr.responseText))
+//                 }else {
+//                     reject('Erro na requisição');
+//                 }
+//             }
+//         }
+//     });
+// }
 
-myPromise()
-    //O parâmetro resolve chama o then o then aguarda a finalização da Promise e é executado caso tenha sucesso
-    .then(function(response) {
+// myPromise()
+//     //O parâmetro resolve chama o then o then aguarda a finalização da Promise e é executado caso tenha sucesso
+//     .then(function(response) {
+//         console.log(response);
+//     })
+//     //O parâmetro rejct chama o catch, o cach aguarda a finalização da Promise e é executado caso haj alguma falha
+//     .catch(function(error) {
+//         console.warn(error);
+//     })
+//-------------------------Utilizando Promises
+
+//-------------------------Utilizando Axios
+//O axios simplifica a forma de fazer requisição
+//Podemos alterar o metódo de get para post, put ou delete
+//Se precisar enviar um parâmetro basta usar a virgula após a url e pasaar o próximo parâmetro
+axios.get('https://api.github.com/users/cjatoba')
+    .then(function(response){
         console.log(response);
+        //Retorna o avatar por exemplo
+        console.log(response.data.avatar_url);
     })
-    //O parâmetro rejct chama o catch, o cach aguarda a finalização da Promise e é executado caso haj alguma falha
-    .catch(function(error) {
+    .catch(function(error){
         console.warn(error);
     })
